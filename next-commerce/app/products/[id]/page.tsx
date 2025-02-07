95a0f93181c0bd4375bc934b7cf3be28d4ef1629
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-07 23:31:02 +0530

    Styled Product detail page using Tailwind CSS

--- next-commerce/app/products/[id]/page.tsx
+++ next-commerce/app/products/[id]/page.tsx
@@ -1,7 +1,7 @@
-import { products } from "@/app/product-data";
+import {products} from "@/app/product-data";
 import NotFoundPage from "@/app/not-found";
 
-export default function ProductDetailPage({ params }: { params: { id: string } }) {
+export default function ProductDetailPage({params}: { params: { id: string } }) {
     const product = products.find((product) => product.id === params.id);
 
     if (!product) {
@@ -9,11 +9,20 @@ export default function ProductDetailPage({ params }: { params: { id: string } }
     }
 
     return (
-        <>
-            <h1>{product.name}</h1>
-            <p>${product.price}</p>
-            <h3>Description:</h3>
-            <p>{product.description}</p>
-        </>
+        <div className={"container mx-auto p-8 flex flex-col md:flex-row"}>
+            <div className={"md:1/2 mb-4 md:mb-0 md:mr-8"}>
+                <img
+                    src={"/" + product.imageUrl}
+                    alt="Product Image"
+                    className={"w-full h-auto rounded-lg shadow-md"}
+                />
+            </div>
+            <div className={"md:w-1/2"}>
+                <h1 className={"text-4xl font-bold mb-4"}>{product.name}</h1>
+                <p className={"text-2xl text-gray-600 mb-6 dark:text-gray-300 "}>${product.price}</p>
+                <h3 className={"text-2xl font-semibold mb-2"}>Description:</h3>
+                <p className={"text-gray-700 dark:text-gray-200 "}>{product.description}</p>
+            </div>
+        </div>
     );
 }
