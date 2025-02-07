88c0ef6849cad5700aeed96c9d9fd79c56d553d7
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-08 00:15:00 +0530

    Styled using Tailwind CSS

---
+++ next-commerce/app/NavBar.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+export default function NavBar () {
+    return (
+        <nav className={"bg-white shadow-md dark:bg-gray-900 dark:text-gray-200"}>
+            <div className={"container mx-auto px-4 py-3 flex justify-between items-center"}>
+                <ul className={"flex space-x-4"}>
+                    <li>
+                        <Link href="/products" className={"font-bold text-gray-700 hover:text-gray-950 dark:text-gray-300 hover:dark:text-gray-100"}>
+                            Products
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/cart" className={"font-bold text-gray-700 hover:text-gray-950 dark:text-gray-300 hover:dark:text-gray-100"}>
+                            Cart
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/checkout" className={"font-bold text-gray-700 hover:text-gray-950 dark:text-gray-300 hover:dark:text-gray-100"}>
+                            Checkout
+                        </Link>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    )
+}
