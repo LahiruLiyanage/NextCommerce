fb51db256d4dc2cc0e1cd0f628af77e578d7c46b
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-07 20:12:52 +0530

    Add Images to the public folder for display in products tab

--- next-commerce/app/ProductsList.tsx
+++ next-commerce/app/ProductsList.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import { Product } from "./product-data";
+import {Product} from "./product-data";
 
-export default function ProductsList ({ products } : { products: Product[] }) {
+export default function ProductsList({products}: { products: Product[] }) {
     return (
         <div>
             {products.map(product => (
                 <div key={product.id}>
-                    <Image src={product.imageUrl} alt="Product Image" width={150} height={150} />
+                    <Image src={'/' + product.imageUrl} alt="Product Image" width={150} height={150}/>
                     <h2>{product.name}</h2>
                     <p>${product.price}</p>
                 </div>
