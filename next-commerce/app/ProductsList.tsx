8d9706db8f1cda67ef4c80ef9fd0121a53700948
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-12 00:43:00 +0530

    Add Products to Cart

--- next-commerce/app/ProductsList.tsx
+++ next-commerce/app/ProductsList.tsx
@@ -3,10 +3,10 @@ import {useState} from "react";
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { Product } from "./product-data";
+import {Product} from "./product-data";
 
 export default function ProductsList({ products, initialCartProducts = [] }: { products: Product[], initialCartProducts: Product[] }) {
-    const [cartProducts, setCartProducts] = useState(initialCartProducts);
+    const [cartProducts, setCartProducts] = useState(initialCartProducts)
 
     async function addToCart(productId: string) {
         const response = await fetch(`http://localhost:3000/api/users/1/cart`, {
@@ -20,6 +20,10 @@ export default function ProductsList({ products, initialCartProducts = [] }: { p
         setCartProducts(updatedCartProducts);
     }
 
+    function productIsInCart(productId: string) {
+        return cartProducts.some(cp => cp.id === productId);
+    }
+
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
             {products.map(product => (
@@ -28,7 +32,8 @@ export default function ProductsList({ products, initialCartProducts = [] }: { p
                     href={`/products/${product.id}`}
                     className="dark:bg-gray-900 bg-white rounded-lg shadow-md dark:hover:shadow-gray-800 p-4 hover:shadow-lg transition duration-300"
                 >
-                    <div className="flex justify-center mb-4 h-48 relative"> {/* Added height and relative positioning */}
+                    <div
+                        className="flex justify-center mb-4 h-48 relative"> {/* Added height and relative positioning */}
                         <Image
                             src={'/' + product.imageUrl}
                             alt="Product image"
@@ -38,10 +43,20 @@ export default function ProductsList({ products, initialCartProducts = [] }: { p
                     </div>
                     <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
                     <p className="dark:text-gray-300 text-gray-600">${product.price}</p>
-                    <button onClick={(e) => {
-                        e.preventDefault();
-                        addToCart(product.id);
-                    }}>Add to Cart</button>
+                    {productIsInCart(product.id)
+                        ? (
+                            <button className="dark:bg-indigo-900 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+                                onClick={(e) => {
+                                e.preventDefault();
+                                console.log('Removing from cart... (Not implemented)');
+                            }}>Remove from Cart</button>
+                        ) : (
+                        <button className="dark:bg-indigo-900 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+                            onClick={(e) => {
+                            e.preventDefault();
+                            addToCart(product.id);
+                        }}>Add to Cart</button>
+                        )}
                 </Link>
             ))}
         </div>
