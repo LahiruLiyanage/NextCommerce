a4bbc6f5e9719da27d940d2d89c4777958df4eef
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-12 23:27:55 +0530

    Add the DELETE Function to remove the product from cart

--- next-commerce/app/ProductsList.tsx
+++ next-commerce/app/ProductsList.tsx
@@ -20,6 +20,18 @@ export default function ProductsList({ products, initialCartProducts = [] }: { p
         setCartProducts(updatedCartProducts);
     }
 
+    async function removeFromCart(productId: string) {
+        const response = await fetch(`http://localhost:3000/api/users/1/cart`, {
+            method: 'DELETE',
+            body: JSON.stringify({
+                productId,
+            }),
+            headers: { 'Content-Type': 'application/json', },
+        });
+        const updatedCartProducts = await response.json();
+        setCartProducts(updatedCartProducts);
+    }
+
     function productIsInCart(productId: string) {
         return cartProducts.some(cp => cp.id === productId);
     }
@@ -48,7 +60,7 @@ export default function ProductsList({ products, initialCartProducts = [] }: { p
                             <button className="dark:bg-indigo-900 hover:shadow-lg bg-blue-500 hover:bg-blue-700 transition duration-300 text-white font-bold py-2 px-4 rounded w-full"
                                 onClick={(e) => {
                                 e.preventDefault();
-                                console.log('Removing from cart... (Not implemented)');
+                                removeFromCart(product.id)
                             }}>Remove from Cart</button>
                         ) : (
                         <button className="dark:bg-indigo-900 hover:shadow-lg bg-blue-500 hover:bg-blue-700 transition duration-300 text-white font-bold py-2 px-4 rounded w-full"
