d1e1f545388bd2e78885a19fbf59964e1b92b2b5
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-04-18 12:42:13 +0530

    Ready to deploy

---
+++ next-commerce/app/api/db.ts
@@ -0,0 +1,29 @@
+import { MongoClient, Db, ServerApiVersion } from 'mongodb';
+
+let cashedClient: MongoClient | null = null;
+let cashedDb: Db | null = null;
+
+export async function connectToDb() {
+    if (cashedClient && cashedDb) {
+        return {client: cashedClient, db: cashedDb};
+    }
+
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.nwrcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    cashedClient = client;
+    cashedDb = client.db('next-commerce-nextjs');
+
+    return { client, db: client.db('next-commerce-nextjs') }
+}
